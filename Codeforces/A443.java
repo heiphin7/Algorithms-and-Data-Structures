cf964c4f5bf01d56b73dd2b374607817bd481e83
rshal <heiphin7and2@gmail.com>
2024-06-21 13:35:50 +0500

    580A Solution

--- Codeforces/A443.java
+++ Codeforces/A443.java
@@ -1,2 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class A443 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String letters = scanner.nextLine();
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < letters.length(); i++) {
+            char curr = letters.charAt(i);
+            if (curr != ' ' && curr != '}' && curr != '{' && curr != ',') {
+                set.add(curr);
+            }
+        }
+
+        System.out.println(set.size());
+    }
 }
