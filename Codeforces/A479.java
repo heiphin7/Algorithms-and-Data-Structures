8a3484937a9be3f5a83b01b5f1c38cf68e33454a
rshal <heiphin7and2@gmail.com>
2024-06-21 13:22:21 +0500

    A479 solution

---
+++ Codeforces/A479.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+    by @heiphin7
+*/
+
+
+public class A479 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // Для решения этой задачи мы просто перебираем все варианты
+        System.out.println(
+                Math.max(
+                        Math.max((a*b*c),(a+b+c)),
+                        Math.max((a * (b+c)), ((a+b) * c))
+                )
+        );
+    }
+}
