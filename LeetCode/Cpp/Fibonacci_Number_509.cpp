6794b6707f026bf10746f55811afc91a447a3751
rshal <heiphin7and2@gmail.com>
2024-06-24 11:59:48 +0500

    Fibonacci number with 0ms with C++

--- LeetCode/Cpp/Fibonacci_Number_509.cpp
+++ LeetCode/Cpp/Fibonacci_Number_509.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
+    unordered_map<int, int> map;
     int fib(int n) {
-        if (n <= 1) {
-            return n;
-        }
-
-        vector<int> dp(n + 1);
-
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for(int i = 2; i <= n; ++i) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
+        if (n <= 1) return n;
+        if (map.find(n) != map.end()) return map[n];
+        map[n] = fib(n-1) + fib(n-2);
+        return map[n];
     }
 };
